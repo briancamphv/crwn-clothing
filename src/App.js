c39657be2daecabfeb85edfb133f9431a5495438
unknown <brian.camp.hv@gmail.com>
2020-12-29 20:44:45 -0600

    broken into more components and added menu item styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './hompage.componet';
+import HomePage from './pages/homepage/homepage.component'
 
 function App() {
   return (
