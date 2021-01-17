73f7a14a1717ada87b9f215e1fc18adb465b42ef
unknown <brian.camp.hv@gmail.com>
2021-01-17 16:57:05 -0600

    added redux logic to replace state logic

--- src/App.js
+++ src/App.js
@@ -5,49 +5,48 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions'
 import { render } from '@testing-library/react';
 import { canConstructResponseFromBodyStream } from 'workbox-core/_private';
 
 
 class App extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubsribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubsribeFromAuth=auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-       
-        const userRef = await createUserProfileDocument(userAuth);
-        
-        
-        userRef.onSnapshot( snapShot => {
 
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+    const {setCurrentUser} = this.props;
+
+    this.unsubsribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+
+        const userRef = await createUserProfileDocument(userAuth);
+
+
+        userRef.onSnapshot(snapShot => {
+
+          setCurrentUser({
+
+            id: snapShot.id,
+            ...snapShot.data()
+
           });
-          console.log(this.state.currentUser);
+          
         })
 
-        
+
 
       } else {
 
-        this.setState({currentUser: userAuth});
+        setCurrentUser(userAuth);
 
       }
-      
-   
+
+
     })
   }
 
@@ -59,7 +58,7 @@ class App extends React.Component {
 
     return (
       <div>
-        <Header currentUser= {this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
@@ -69,7 +68,11 @@ class App extends React.Component {
     );
 
   }
-  
+
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
