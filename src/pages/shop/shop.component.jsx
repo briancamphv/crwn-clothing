3e4b0675ac99c1e29db0e9f8d0af0a70a0df42d0
unknown <brian.camp.hv@gmail.com>
2021-02-13 08:24:03 -0600

    added database calls to shop data on firebase

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -22,11 +22,16 @@ class ShopPage extends React.Component {
 
         const {updateCollections} = this.props;
         const collectionRef = firestore.collection('collections');
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+        collectionRef.get().then(snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
             this.setState({loading: false})
         })
+        // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+        //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        //     updateCollections(collectionsMap);
+        //     this.setState({loading: false})
+        // })
 
     }
 
